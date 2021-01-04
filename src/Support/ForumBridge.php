cac0825661d6a472fd1cebef53f3fe0b98aae27f
Nicolas Perrenoud <mail@nicu.ch>
2021-01-04 04:16:11 +0200

    Default forum url

--- src/Support/ForumBridge.php
+++ src/Support/ForumBridge.php
@@ -299,7 +299,7 @@ class ForumBridge
 
     public static function url(?string $type = null, $value = null, $value2 = null): string
     {
-        $baseUrl = get_config('forum_url');
+        $baseUrl = get_config('forum_url', 'https://forum.etoa.ch/');
         if ($type == 'board') {
             return $baseUrl . '/index.php?page=Board&amp;boardID=' . $value;
         }
