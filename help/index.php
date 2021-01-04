8d5b4b81ce6014051a9a5feba8957038b491fa5a
Nicolas Perrenoud <mail@nicu.ch>
2021-01-04 10:00:07 +0200

    PDO exception handling

--- help/index.php
+++ help/index.php
@@ -5,50 +5,57 @@ use App\TemplateEngine;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-session_start();
+try {
 
-// Templating engine
-$tpl = new TemplateEngine();
+    // Session
+    session_start();
 
-// Login
-ob_start();
-$auth = false;
-if (isset($_SESSION['etoahelp']['username']) && isset($_SESSION['etoahelp']['uid']) && $_SESSION['etoahelp']['uid'] > 0) {
-	$auth = true;
-	echo "<p>Eingeloggt als <b>" . $_SESSION['etoahelp']['username'] . "</b></p>
-		<p>
-		<a href=\"" . ForumBridge::url('account') . "\">Accountverwaltung</a><br/>
-        <a href=\"?page=user&amp;id=" . $_SESSION['etoahelp']['uid'] . "\">Benutzerprofil</a><br/>
-        <a href=\"logout\">Logout</a><br/>
-		</p>";
+    // Templating engine
+    $tpl = new TemplateEngine();
+
+    // Login
+    ob_start();
+    $auth = false;
+    if (isset($_SESSION['etoahelp']['username']) && isset($_SESSION['etoahelp']['uid']) && $_SESSION['etoahelp']['uid'] > 0) {
+        $auth = true;
+        echo "<p>Eingeloggt als <b>" . $_SESSION['etoahelp']['username'] . "</b></p>
+            <p>
+            <a href=\"" . ForumBridge::url('account') . "\">Accountverwaltung</a><br/>
+            <a href=\"?page=user&amp;id=" . $_SESSION['etoahelp']['uid'] . "\">Benutzerprofil</a><br/>
+            <a href=\"logout\">Logout</a><br/>
+            </p>";
+    }
+    $tpl->assign("loginbox", ob_get_clean());
+
+    // Content
+    $page = isset($_GET['page']) ? $_GET['page'] : 'index';
+    if (preg_match('/^[a-z0-9_\/\-]+$/i', $page) > 0) {
+        $pagepath = __DIR__ . "/../content/help/$page.php";
+        if (is_file($pagepath)) {
+            ob_start();
+            require $pagepath;
+            $content = ob_get_clean();
+            $tpl->assign("content", $content);
+            if (isset($header_content)) {
+                $tpl->assign("header_content", $header_content);
+            }
+        } else {
+            http_response_code(404);
+            $tpl->assign("title", "Fehler");
+            $tpl->assign("error", "Seite wurde nicht gefunden!");
+        }
+    } else {
+        http_response_code(400);
+        $tpl->assign("title", "Fehler");
+        $tpl->assign("error", "Ungültige Abfrage!");
+    }
+
+    // Site nbame
+    $tpl->assign("site_title", $site_title ?? ucfirst($page));
+
+    // Render
+    $tpl->render('layouts/help.html');
+
+} catch (\PDOException $ex) {
+    abort($ex->getMessage(), 'Datenbankfehler');
 }
-$tpl->assign("loginbox", ob_get_clean());
-
-// Content
-$page = isset($_GET['page']) ? $_GET['page'] : 'index';
-if (preg_match('/^[a-z0-9_\/\-]+$/i', $page) > 0) {
-    $pagepath = __DIR__ . "/../content/help/$page.php";
-	if (is_file($pagepath)) {
-		ob_start();
-		require $pagepath;
-        $content = ob_get_clean();
-		$tpl->assign("content", $content);
-		if (isset($header_content)) {
-			$tpl->assign("header_content", $header_content);
-		}
-	} else {
-        http_response_code(404);
-		$tpl->assign("title", "Fehler");
-		$tpl->assign("error", "Seite wurde nicht gefunden!");
-	}
-} else {
-    http_response_code(400);
-    $tpl->assign("title", "Fehler");
-	$tpl->assign("error", "Ungültige Abfrage!");
-}
-
-// Site nbame
-$tpl->assign("site_title", $site_title ?? ucfirst($page));
-
-// Render
-$tpl->render('layouts/help.html');
--- index.php
+++ index.php
@@ -14,54 +14,60 @@ use App\Widgets\MainMenu;
 
 require __DIR__ . '/vendor/autoload.php';
 
-// Konfiguration laden
-session_start();
+try {
 
-// Maintenance
-if (get_config('maintenance_mode') == 1) {
-    include('_maintenance/index.html');
-    exit;
-}
+    // Session
+    session_start();
 
-// Templating engine
-$tpl = new TemplateEngine();
-
-// Page
-$page = isset($_GET['page']) ? $_GET['page'] : 'news';
-$tpl->assign('page', $page);
-if (preg_match('/^[a-z0-9_\/\-]+$/i', $page) > 0) {
-    $pagepath = __DIR__ . "/content/$page.php";
-    if (is_file($pagepath)) {
-        ob_start();
-        require $pagepath;
-        $ob = ob_get_clean();
-        $tpl->assign("content", $ob);
-    } else {
-        http_response_code(404);
-        $tpl->assign('title', 'Fehler');
-        $tpl->assign("error", "Seite wurde nicht gefunden!");
+    // Maintenance mode
+    if (get_config('maintenance_mode') == 1) {
+        include('_maintenance/index.html');
+        exit;
     }
-} else {
-    http_response_code(400);
-    $tpl->assign('title', 'Fehler');
-    $tpl->assign("error", "Ungültige Abfrage!");
+
+    // Templating engine
+    $tpl = new TemplateEngine();
+
+    // Page
+    $page = isset($_GET['page']) ? $_GET['page'] : 'news';
+    $tpl->assign('page', $page);
+    if (preg_match('/^[a-z0-9_\/\-]+$/i', $page) > 0) {
+        $pagepath = __DIR__ . "/content/$page.php";
+        if (is_file($pagepath)) {
+            ob_start();
+            require $pagepath;
+            $ob = ob_get_clean();
+            $tpl->assign("content", $ob);
+        } else {
+            http_response_code(404);
+            $tpl->assign('title', 'Fehler');
+            $tpl->assign("error", "Seite wurde nicht gefunden!");
+        }
+    } else {
+        http_response_code(400);
+        $tpl->assign('title', 'Fehler');
+        $tpl->assign("error", "Ungültige Abfrage!");
+    }
+
+    // Widgets
+    $tpl->assign('gameLogin', new GameLogin());
+    $tpl->assign('mainMenu', new MainMenu());
+    $tpl->assign('infobox', new InfoBox());
+
+    // Text blocks
+    $tpl->assign('votebanner', get_config('buttons'));
+    $tpl->assign('adds', get_config('adds'));
+    $tpl->assign('footerJs', get_config('footer_js'));
+    $tpl->assign('headerJs', get_config('indexjscript'));
+
+    // Site nbame
+    $tpl->assign("site_title", $site_title ?? ucfirst($page));
+
+    // Render
+    $tpl->render('layouts/site.html');
+
+    dbclose();
+
+} catch (\PDOException $ex) {
+    abort($ex->getMessage(), 'Datenbankfehler');
 }
-
-// Widgets
-$tpl->assign('gameLogin', new GameLogin());
-$tpl->assign('mainMenu', new MainMenu());
-$tpl->assign('infobox', new InfoBox());
-
-// Text blocks
-$tpl->assign('votebanner', get_config('buttons'));
-$tpl->assign('adds', get_config('adds'));
-$tpl->assign('footerJs', get_config('footer_js'));
-$tpl->assign('headerJs', get_config('indexjscript'));
-
-// Site nbame
-$tpl->assign("site_title", $site_title ?? ucfirst($page));
-
-// Render
-$tpl->render('layouts/site.html');
-
-dbclose();
