f7c022008dd7adbecb3efbde2575af6bc08e5ac3
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-08-22 15:54:17 +0300

    Twig test

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,8 @@ use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 // Load libraries
 require __DIR__ . '/vendor/autoload.php';
@@ -23,17 +25,32 @@ $app = AppFactory::create();
 
 // Error handling
 $app->addErrorMiddleware($debug, true, true);
-ini_set('display_errors', '0');
+if (!$debug) {
+    ini_set('display_errors', '0');
+}
 
 // Base path
 if (isset($_ENV['APP_BASEPATH'])) {
     $app->setBasePath($_ENV['APP_BASEPATH']);
 }
 
+// Add Twig-View Middleware
+$twig = Twig::create(__DIR__ . '/templates', [
+    'cache' => false,
+]);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
+
+$app->get('/test', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'test.html', [
+        'header_img' => 'news.png',
+    ]);
+})->setName('test');
 
 // Run app
 $app->run();
