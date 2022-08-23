6b54e04a9249f4ccdff0939d173e7d6673f03e60
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-08-23 12:39:22 +0300

    cleanup index

--- index.php
+++ index.php
@@ -7,11 +7,6 @@
 // als Maturaarbeit '04 am Gymnasium Oberaargau	//
 //////////////////////////////////////////////////
 
-use App\TemplateEngine;
-use App\Widgets\GameLogin;
-use App\Widgets\InfoBox;
-use App\Widgets\MainMenu;
-
 require __DIR__ . '/vendor/autoload.php';
 
 try {
@@ -26,36 +21,6 @@ try {
         include('_maintenance/index.html');
         exit;
     }
-
-    // Templating engine
-    $tpl = new TemplateEngine();
-
-    // Page
-    $page = isset($_GET['page']) ? $_GET['page'] : 'news';
-    $tpl->assign('page', $page);
-    if (preg_match('/^[a-z0-9_\/\-]+$/i', $page) > 0) {
-        $pagepath = __DIR__ . "/content/$page.php";
-        if (is_file($pagepath)) {
-            ob_start();
-            require $pagepath;
-            $ob = ob_get_clean();
-            $tpl->assign("content", $ob);
-        } else {
-            http_response_code(404);
-            $tpl->assign('title', 'Fehler');
-            $tpl->assign("error", "Seite wurde nicht gefunden!");
-        }
-    } else {
-        http_response_code(400);
-        $tpl->assign('title', 'Fehler');
-        $tpl->assign("error", "Ungültige Abfrage!");
-    }
-
-    // Site nbame
-    $tpl->assign("site_title", $site_title ?? ucfirst($page));
-
-    // Render
-    $tpl->render('layouts/site.html');
 
     dbclose();
 } catch (\PDOException $ex) {
