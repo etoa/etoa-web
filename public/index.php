479f8e48007df4da40ca2d38462ceb325a7537fe
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-08-29 09:35:41 +0300

    simplify bootstrap file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,90 @@
 <?php
 
-require __DIR__ . '/../bootstrap.php';
+
+declare(strict_types=1);
+
+use App\Authentication\ForumAuthenticator;
+use App\Middleware\HttpsRedirectMiddleware;
+use App\Service\ConfigService;
+use App\Support\Database\DatabaseEntityManagerInitializer;
+use App\Support\TwigConfigurationInitializer;
+use Carbon\Carbon;
+use DI\Bridge\Slim\Bridge;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Dotenv\Dotenv;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Tuupola\Middleware\HttpBasicAuthentication;
+
+define('APP_DIR', __DIR__ . '/../');
+
+// Load libraries
+require APP_DIR . '/vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(APP_DIR);
+$dotenv->safeLoad();
+
+// Define environment and debug mode
+$environment = ($_ENV['APP_ENV'] ?? 'production');
+$debug = !in_array($environment, ['prod', 'production']);
+
+// Define locale
+Carbon::setLocale($_ENV['LOCALE'] ?? 'de');
+
+$twig = TwigConfigurationInitializer::create($debug, !$debug);
+
+// Init app
+$container = new Container();
+$container->set(Twig::class, fn () => $twig);
+$container->set('session', fn () => new \SlimSession\Helper());
+$container->set(EntityManager::class, DatabaseEntityManagerInitializer::initialize($debug));
+$container->set(ConfigService::class, static function (Container $c) {
+    return new ConfigService($c->get(EntityManager::class));
+});
+
+$app = Bridge::create($container);
+
+// Error handling
+$app->addErrorMiddleware($debug, true, true);
+if (!$debug) {
+    ini_set('display_errors', '0');
+}
+
+// HTTPS redirect
+if (!$debug) {
+    $app->add(HttpsRedirectMiddleware::class);
+}
+
+// Base path
+$app->setBasePath($_ENV['APP_BASEPATH'] ?? getAppBasePath());
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+// Add session management
+$app->add(new \Slim\Middleware\Session());
+
+// Routing
+if (file_exists(APP_DIR . '/storage/maintenance')) {
+    $app->any('/{path:.*}', MaintenancePageController::class);
+} else {
+    $app->group('', FrontendRoutes::class);
+    $app->group('/admin', BackendRoutes::class)
+        ->add(new HttpBasicAuthentication([
+            "realm" => "EtoA Login Administration",
+            "authenticator" => new ForumAuthenticator,
+            "before" => function (Slim\Psr7\Request $request, array $arguments) {
+                return $request
+                    ->withAttribute("user", $arguments["user"])
+                    ->withAttribute("password", $arguments["password"]);
+            },
+            'secure' => false,
+        ]));
+
+    $app->any('/{path:.*}', PageNotFoundController::class);
+}
+
+// Run app
+$app->run();
