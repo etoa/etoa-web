3964a95be9be11f5f92ff4be6d253b0a3cecaf65
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-09-06 13:56:48 +0300

    Use files directory for public files

--- src/Controllers/Backend/FilesController.php
+++ src/Controllers/Backend/FilesController.php
@@ -21,7 +21,7 @@ use Slim\Views\Twig;
 
 class FilesController extends BackendController
 {
-    private const DIR = '/pub';
+    private const DIR = '/files';
     private const BASE_PATH = APP_DIR . '/public' . self::DIR;
     private const PUB_PATH = self::DIR;
 
@@ -137,7 +137,7 @@ class FilesController extends BackendController
             $this->log->info('Renamed file ' . $path . ' to ' . $destination);
             $this->deleteEmptyDirecytories();
             $this->setSessionMessage('success', 'Datei umbenannt.');
-        } catch (FilesystemException|UnableToMoveFile $exception) {
+        } catch (FilesystemException | UnableToMoveFile $exception) {
             $this->setSessionMessage('error', 'Datei konnte nicht umbenannt werden: ' . $exception->getMessage());
         }
 
@@ -161,7 +161,7 @@ class FilesController extends BackendController
                         $this->filesystem->deleteDirectory($dir);
                         ++$deleted;
                         $this->log->debug('Deleted directory ' . $dir);
-                    } catch (FilesystemException|UnableToDeleteDirectory $exception) {
+                    } catch (FilesystemException | UnableToDeleteDirectory $exception) {
                         $this->log->error('Unable to delete directory ' . $dir . ': ' . $exception->getMessage());
                     }
                 }
@@ -193,7 +193,7 @@ class FilesController extends BackendController
             $this->log->info('Deleted file ' . $path);
             $this->deleteEmptyDirecytories();
             $this->setSessionMessage('success', 'Datei gelöscht.');
-        } catch (FilesystemException|UnableToDeleteFile $exception) {
+        } catch (FilesystemException | UnableToDeleteFile $exception) {
             $this->setSessionMessage('error', 'Datei konnte nicht gelöscht werden: ' . $exception->getMessage());
         }
 
