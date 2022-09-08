c34b0501ac18c42a45a891e6535ef9b33c687f00
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2022-09-08 14:30:06 +0300

    Basic round login form

--- resources/js/frontend_ng.js
+++ resources/js/frontend_ng.js
@@ -1,1 +1,31 @@
 import '../scss/frontend_ng.scss'
+
+function updateLoginFormAction() {
+    const loginRound = document.getElementById('loginround').value;
+    if (loginRound == '') {
+        alert('Du hast keine Runde ausgewählt.');
+    } else {
+        document.getElementById('loginform').action = loginRound;
+    }
+}
+
+function rememberLoginRound() {
+    const loginRound = document.getElementById('loginround').value;
+    localStorage.setItem('round', loginRound);
+}
+
+function restoreLoginRound() {
+    const loginRound = localStorage.getItem('round', loginRound);
+    if (Object.values(document.getElementById('loginround').options).map(o => o.attributes.value.value).includes(loginRound)) {
+        document.getElementById('loginround').value = loginRound;
+    }
+}
+
+window.onload = function () {
+    restoreLoginRound();
+    updateLoginFormAction();
+    document.getElementById("loginround").addEventListener('change', () => {
+        updateLoginFormAction();
+        rememberLoginRound();
+    });
+}
--- src/Controllers/FrontendNg/HomeController.php
+++ src/Controllers/FrontendNg/HomeController.php
@@ -3,14 +3,23 @@
 namespace App\Controllers\FrontendNg;
 
 use App\Controllers\AbstractController;
+use App\Support\GameLoginFormService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private GameLoginFormService $loginForm,
+    ) {
+    }
+
     public function __invoke(Request $request, Response $response, Twig $view): Response
     {
-        return $view->render($response, 'frontend_ng/home.html', []);
+        return $view->render($response, 'frontend_ng/home.html', [
+            'loginform' => $this->loginForm->createLoginFormData(),
+            'rounds' => $this->loginForm->getRounds(),
+        ]);
     }
 }
